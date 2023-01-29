936079379f01dd1df11654265e7036308dac330a
Arch <mateus.azeredo.torres@gmail.com>
2023-01-29 01:14:26 -0300

    Part 1 Domain Modeling: Bare minimum TDD of business domain rules

---
+++ tests/unit/test_model.py
@@ -0,0 +1,20 @@
+from datetime import date, timedelta
+import pytest
+
+# inicialmente esse statement vai estar comentado para o teste falhar
+# from model import ...
+
+def test_allocating_to_a_batch_reduces_the_available_quantity():
+    pytest.fail("TO IMPLEMENT")
+
+def test_allocating_to_a_batch_fails_if_available_quantity_is_less():
+    pytest.fail("TO IMPLEMENT")
+
+def test_allocating_to_a_batch_fails_if_for_the_same_order_line():
+    pytest.fail("TO IMPLEMENT")
+
+def test_allocating_to_a_batch_preferences_warehouse_stock():
+    pytest.fail("TO IMPLEMENT")
+
+def test_allocating_to_a_batch_ordered_by_eta():
+    pytest.fail("TO IMPLEMENT")
