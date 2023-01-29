cc1a045a2cc35c918dda792e2af853b60a07473c
Arch <mateus.azeredo.torres@gmail.com>
2023-01-29 01:18:47 -0300

    Part 1 Domain Modeling: Some more unit tests

--- tests/unit/test_model.py
+++ tests/unit/test_model.py
@@ -13,8 +13,15 @@ def test_allocating_to_a_batch_fails_if_available_quantity_is_less():
 def test_allocating_to_a_batch_fails_if_for_the_same_order_line():
     pytest.fail("TO IMPLEMENT")
 
+def test_can_allocate_if_available_quantity_is_greater():
+    pytest.fail("TO IMPLEMENT")
+
+def test_can_allocate_if_available_quantity_is_equal():
+    pytest.fail("TO IMPLEMENT")
+
 def test_allocating_to_a_batch_preferences_warehouse_stock():
     pytest.fail("TO IMPLEMENT")
 
 def test_allocating_to_a_batch_ordered_by_eta():
     pytest.fail("TO IMPLEMENT")
+
