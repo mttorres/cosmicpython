20b6d04e34c1d6ba7e85cd00f7f67964ed4e255b
Arch <mateus.azeredo.torres@gmail.com>
2023-01-30 21:40:16 -0300

    Part 1 Domain Modeling: DE-Allocation rules implementation that conforms to the previous test

--- src/allocation/domain/model.py
+++ src/allocation/domain/model.py
@@ -17,9 +17,14 @@ class Batch:
         self.sku = sku
         self.eta = eta
         self.available_quantity = qty
+        self._allocations = set()
 
     def can_allocate(self, line: OrderLine) -> bool:
         return self.sku == line.sku and self.available_quantity >= line.qty
 
     def allocate(self, line: OrderLine):
         self.available_quantity -= line.qty
+
+    def deallocate(self, line: OrderLine):
+        if line in self._allocations:
+            self._allocations.remove(line)
