93de0fb13fbd45efce8d157a32de994316edfdb4
Arch <mateus.azeredo.torres@gmail.com>
2023-01-30 23:21:49 -0300

    Part 1 Domain Modeling: Model confirming to money rules

--- src/commons/money.py
+++ src/commons/money.py
@@ -1,7 +1,23 @@
 from dataclasses import dataclass
 
 
+
 @dataclass(frozen=True)
 class Money:
     currency: str
     value: int
+
+    def __add__(self, other):
+        if other.currency != self.currency:
+            raise ValueError(f"Cannot add {self.currency} to {other.currency}")
+        return Money(self.currency, self.value + other.value)
+
+    def __sub__(self, other):
+        if other.currency != self.currency:
+            raise ValueError(f"Cannot subtract {self.currency} to {other.currency}")
+        if other.value > self.value:
+            raise ValueError(f"Subtract operation cannot result in negative values")
+        return Money(self.currency, self.value - other.value)
+
+    def __mul__(self, other):
+        return Money(self.currency, self.value*other)
