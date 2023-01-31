a5f7f2991589aefa5a1c96ab4e57a6ee216e9831
Arch <mateus.azeredo.torres@gmail.com>
2023-01-30 21:34:27 -0300

    Part 1 Domain Modeling: Allocation rules implementation that conforms to the previous test

--- src/allocation/domain/model.py
+++ src/allocation/domain/model.py
@@ -18,5 +18,8 @@ class Batch:
         self.eta = eta
         self.available_quantity = qty
 
+    def can_allocate(self, line: OrderLine) -> bool:
+        return self.sku == line.sku and self.available_quantity >= line.qty
+
     def allocate(self, line: OrderLine):
         self.available_quantity -= line.qty
