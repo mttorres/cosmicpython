d2a47e042c0556c1930c2465423c2b6bc6ce721e
Arch <mateus.azeredo.torres@gmail.com>
2023-02-03 20:37:23 -0300

    Part 1 Repository Pattern: Repository implementation compliant to previous tests

--- src/allocation/adapters/repository.py
+++ src/allocation/adapters/repository.py
@@ -3,10 +3,24 @@ from src.allocation.domain import model
 
 
 class AbstractRepository(abc.ABC):
-    @abc.abstractmethod  # (1)
+    @abc.abstractmethod
     def add(self, batch: model.Batch):
-        raise NotImplementedError  # (2)
+        raise NotImplementedError
 
     @abc.abstractmethod
     def get(self, reference) -> model.Batch:
         raise NotImplementedError
+
+
+class SqlAlchemyRepository(AbstractRepository):
+    def __init__(self, session):
+        self.session = session
+
+    def add(self, batch):
+        self.session.add(batch)
+
+    def get(self, reference):
+        return self.session.query(model.Batch).filter_by(reference=reference).one()
+
+    def list(self):
+        return self.session.query(model.Batch).all()
