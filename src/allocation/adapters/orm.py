8cfb58d53da49bb34d676a586e40c8a54a7089ea
Arch <mateus.azeredo.torres@gmail.com>
2023-02-03 20:31:55 -0300

    Part 1 Repository Pattern: Complete ORM implementation

--- src/allocation/adapters/orm.py
+++ src/allocation/adapters/orm.py
@@ -15,6 +15,33 @@ order_lines = Table(
     Column("orderid", String(255)),
 )
 
+batches = Table(
+    "batches",
+    metadata,
+    Column("id", Integer, primary_key=True, autoincrement=True),
+    Column("reference", String(255)),
+    Column("sku", String(255)),
+    Column("_purchased_quantity", Integer, nullable=False),
+    Column("eta", Date, nullable=True),
+)
+
+allocations = Table(
+    "allocations",
+    metadata,
+    Column("id", Integer, primary_key=True, autoincrement=True),
+    Column("orderline_id", ForeignKey("order_lines.id")),
+    Column("batch_id", ForeignKey("batches.id")),
+)
+
 
 def start_mappers():
     lines_mapper = mapper_registry.map_imperatively(model.OrderLine, order_lines)
+    mapper_registry.map_imperatively(
+        model.Batch,
+        batches,
+        properties={
+            "_allocations": relationship(
+                lines_mapper, secondary=allocations, collection_class=set,
+            )
+        },
+    )
