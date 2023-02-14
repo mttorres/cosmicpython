cd1655d9b3e3cc7b2cdf7e8cc5ba817770cf950d
Arch <mateus.azeredo.torres@gmail.com>
2023-02-14 20:46:59 -0300

    Part 1 Flask and Service Layer: Refactoring flask_app as we introduced a service layer

--- src/allocation/entrypoints/flask_app.py
+++ src/allocation/entrypoints/flask_app.py
@@ -6,31 +6,24 @@ from src.allocation import config
 from src.allocation.domain import model
 from src.allocation.adapters import orm
 from src.allocation.adapters import repository
+from src.allocation.service_layer import services
 
 orm.start_mappers()
 get_session = sessionmaker(bind=create_engine(config.get_postgres_uri()))
 app = Flask(__name__)
 
 
-def is_valid_sku(sku, batches):
-    return sku in {b.sku for b in batches}
-
-
 @app.route("/allocate", methods=["POST"])
 def allocate_endpoint():
     db_session = get_session()
-    batches = repository.SqlAlchemyRepository(db_session).list()
+    repo = repository.SqlAlchemyRepository(db_session)
     line = model.OrderLine(
         request.json["orderid"], request.json["sku"], request.json["qty"],
     )
 
-    if not is_valid_sku(line.sku, batches):
-        return {"message": f"Invalid sku {line.sku}"}, 400
-
     try:
-        batch_ref = model.allocate(line, batches)
-    except model.OutOfStock as e:
+        batchref = services.allocate(line, repo, db_session)
+    except (model.OutOfStock, services.InvalidSku) as e:
         return {"message": str(e)}, 400
 
-    db_session.commit()
-    return {"batchref": batch_ref}, 201
+    return {"batchref": batchref}, 201
