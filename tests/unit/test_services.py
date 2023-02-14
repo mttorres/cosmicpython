8547f999bee5ba603977818bb2a31f4a3cd00495
Arch <mateus.azeredo.torres@gmail.com>
2023-02-14 20:00:52 -0300

    Part 1 Flask and Service Layer: Adding an equivalent unit test to e2e test_allocations_are_persisted

--- tests/unit/test_services.py
+++ tests/unit/test_services.py
@@ -5,6 +5,13 @@ from src.allocation.adapters.repository import FakeRepository
 from src.allocation.service_layer import services
 
 
+class FakeSession:
+    committed = False
+
+    def commit(self):
+        self.committed = True
+
+
 def test_returns_allocation():
     line = model.OrderLine("o1", "COMPLICATED-LAMP", 10)
     batch = model.Batch("b1", "COMPLICATED-LAMP", 100, eta=None)
@@ -21,3 +28,13 @@ def test_error_for_invalid_sku():
 
     with pytest.raises(services.InvalidSku, match="Invalid sku NONEXISTENTSKU"):
         services.allocate(line, repo, FakeSession())
+
+
+def test_commits():
+    line = model.OrderLine("o1", "OMINOUS-MIRROR", 10)
+    batch = model.Batch("b1", "OMINOUS-MIRROR", 100, eta=None)
+    repo = FakeRepository([batch])
+    session = FakeSession()
+
+    services.allocate(line, repo, session)
+    assert session.committed is True
