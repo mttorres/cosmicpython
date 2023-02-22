ff5886ba14595b547a43efefed6c54d7f0d74641
Arch <mateus.azeredo.torres@gmail.com>
2023-02-22 19:25:20 -0300

    Part 1 Part 1 TDD in High Gear and Low Gear: Removing domain dependencies from test_services

--- tests/unit/test_services.py
+++ tests/unit/test_services.py
@@ -1,8 +1,6 @@
-from datetime import date
-
+from datetime import date, timedelta
 import pytest
 
-from src.allocation.domain import model
 from src.allocation.adapters.repository import FakeRepository
 from src.allocation.service_layer import services
 
@@ -23,40 +21,36 @@ def test_add_batch():
 
 
 def test_returns_allocation():
-    batch = model.Batch("b1", "COMPLICATED-LAMP", 100, eta=None)
-    repo = FakeRepository([batch])
-
+    repo, session = FakeRepository([]), FakeSession()
+    services.add_batch("b1", "COMPLICATED-LAMP", 100, None, repo, session)
     result = services.allocate("o1", "COMPLICATED-LAMP", 100, repo, FakeSession())
     assert result == "b1"
 
 
 def test_error_for_invalid_sku():
-    line = model.OrderLine("o1", "NONEXISTENTSKU", 10)
-    batch = model.Batch("b1", "AREALSKU", 100, eta=None)
-    repo = FakeRepository([batch])
+    repo, session = FakeRepository([]), FakeSession()
+    services.add_batch("b1", "AREALSKU", 100, None, repo, session)
 
     with pytest.raises(services.InvalidSku, match="Invalid sku NONEXISTENTSKU"):
-        services.allocate(line.orderid, line.sku, line.qty, repo, FakeSession())
+        services.allocate("o1", "NONEXISTENTSKU", 10,  repo, FakeSession())
 
 
 def test_commits():
-    line = model.OrderLine("o1", "OMINOUS-MIRROR", 10)
-    batch = model.Batch("b1", "OMINOUS-MIRROR", 100, eta=None)
-    repo = FakeRepository([batch])
-    session = FakeSession()
-
-    services.allocate(line.orderid, line.sku, line.qty, repo, session)
+    repo, session = FakeRepository([]), FakeSession()
+    services.add_batch("b1", "OMINOUS-MIRROR", 100, None, repo, session)
+    services.allocate("o1", "OMINOUS-MIRROR", 10, repo, session)
     assert session.committed is True
 
 
 def test_returns_deallocation():
-    line = model.OrderLine("o1", "COMPLICATED-LAMP", 10)
-    batch = model.Batch("b1", "COMPLICATED-LAMP", 100, eta=None)
-    non_allocated_batch = model.Batch("b2", "COMPLICATED-LAMP", 100, eta=date.today())
-    repo = FakeRepository([batch, non_allocated_batch])
-    services.allocate(line.orderid, line.sku, line.qty, repo, FakeSession())
+    repo = FakeRepository([])
+    # allocated batch
+    services.add_batch("b1", "COMPLICATED-LAMP", 100, None, repo, FakeSession())
+    # non-allocated batch
+    services.add_batch("b2", "COMPLICATED-LAMP", 100, date.today(), repo, FakeSession())
+    services.allocate("o1", "COMPLICATED-LAMP", 10, repo, FakeSession())
 
-    results = services.deallocate(line.orderid, line.sku, repo, FakeSession())
+    results = services.deallocate("o1", "COMPLICATED-LAMP", repo, FakeSession())
     assert "b1" in results
     assert "b2" not in results
 
@@ -64,8 +58,7 @@ def test_returns_deallocation():
 def test_deallocate_persists_decrement_to_the_available_quantity():
     repo = FakeRepository([])
     services.add_batch("b1", "BLUE-PLINTH", 100, None, repo, FakeSession())
-    line = model.OrderLine("o1", "BLUE-PLINTH", 10)
-    services.allocate(line.orderid, line.sku, line.qty, repo, FakeSession())
+    services.allocate("o1", "BLUE-PLINTH", 10, repo, FakeSession())
 
     batch = repo.get(reference="b1")
     assert batch.available_quantity == 90
@@ -76,8 +69,7 @@ def test_deallocate_persists_decrement_to_the_available_quantity():
 def test_deallocate_decrements_correct_quantity():
     repo = FakeRepository([])
     services.add_batch("b1", "BLUE-PLINTH", 100, None, repo, FakeSession())
-    line = model.OrderLine("o1", "BLUE-PLINTH", 10)
-    services.allocate(line.orderid, line.sku, line.qty, repo, FakeSession())
+    services.allocate("o1", "BLUE-PLINTH", 10, repo, FakeSession())
     batch = repo.get(reference="b1")
     services.add_batch("b2", "RED-PLINTH", 100, None, repo, FakeSession())
     other_batch = repo.get(reference="b2")
@@ -90,14 +82,15 @@ def test_deallocate_decrements_correct_quantity():
 
 
 def test_prefers_warehouse_batches_to_shipments():
-    in_stock_batch = Batch("in-stock-batch", "RETRO-CLOCK", 100, eta=None)
-    shipment_batch = Batch("shipment-batch", "RETRO-CLOCK", 100, eta=tomorrow)
-    repo = FakeRepository([in_stock_batch, shipment_batch])
+    repo = FakeRepository([])
     session = FakeSession()
+    # in stock batch
+    services.add_batch("in-stock-batch", "RETRO-CLOCK", 100, None, repo, session)
+    # shipment batch
+    services.add_batch("shipment-batch", "RETRO-CLOCK", 100, date.today() + timedelta(days=1), repo, session)
 
-    line = OrderLine('oref', "RETRO-CLOCK", 10)
+    services.allocate('oref', "RETRO-CLOCK", 10, repo, session)
 
-    services.allocate(line, repo, session)
+    assert repo.get("in-stock-batch").available_quantity == 90
+    assert repo.get("shipment-batch").available_quantity == 100
 
-    assert in_stock_batch.available_quantity == 90
-    assert shipment_batch.available_quantity == 100
