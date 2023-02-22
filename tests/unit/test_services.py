16a943c98a30588e259f7c23eda4e3335127e78e
Arch <mateus.azeredo.torres@gmail.com>
2023-02-22 18:35:59 -0300

    Part 1 Part 1 TDD in High Gear and Low Gear: TDD for add_batch

--- tests/unit/test_services.py
+++ tests/unit/test_services.py
@@ -15,6 +15,13 @@ class FakeSession:
         self.committed = True
 
 
+def test_add_batch():
+    repo, session = FakeRepository([]), FakeSession()
+    services.add_batch("b1", "CRUNCHY-ARMCHAIR", 100, None, repo, session)
+    assert repo.get("b1") is not None
+    assert session.committed
+
+
 def test_returns_allocation():
     batch = model.Batch("b1", "COMPLICATED-LAMP", 100, eta=None)
     repo = FakeRepository([batch])
