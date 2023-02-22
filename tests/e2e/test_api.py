88200581c49367e80be372071b7d6e09216da1e2
Arch <mateus.azeredo.torres@gmail.com>
2023-02-22 19:51:01 -0300

    Part 1 Part 1 TDD in High Gear and Low Gear: Removing add_stock fixtures from e2e tests

--- tests/e2e/test_api.py
+++ tests/e2e/test_api.py
@@ -21,22 +21,27 @@ def random_orderid(name=""):
     return f"order-{name}-{random_suffix()}"
 
 
+def put_to_add_batch(ref, sku, qty, eta):
+    url = config.get_api_url()
+    r = requests.put(
+        f"{url}/batch", json={"ref": ref, "sku": sku, "qty": qty, "eta": eta}
+    )
+    assert r.status_code == 201
+
+
+@pytest.mark.usefixtures("postgres_db")
 @pytest.mark.usefixtures("restart_api")
-def test_happy_path_returns_201_and_allocated_batch(add_stock):
+def test_happy_path_returns_201_and_allocated_batch():
     sku, othersku = random_sku(), random_sku("other")
     earlybatch = random_batchref("1")
     laterbatch = random_batchref("2")
     otherbatch = random_batchref("3")
-    add_stock(
-        [
-            (laterbatch, sku, 100, "2011-01-02"),
-            (earlybatch, sku, 100, "2011-01-01"),
-            (otherbatch, othersku, 100, None),
-        ]
-    )
+    put_to_add_batch(laterbatch, sku, 100, "2011-01-02")
+    put_to_add_batch(earlybatch, sku, 100, "2011-01-01")
+    put_to_add_batch(otherbatch, othersku, 100, None)
     data = {"orderid": random_orderid(), "sku": sku, "qty": 3}
-    url = config.get_api_url()
 
+    url = config.get_api_url()
     r = requests.post(f"{url}/allocate", json=data)
 
     assert r.status_code == 201
@@ -53,13 +58,13 @@ def test_unhappy_path_returns_400_and_error_message():
     assert r.json()["message"] == f"Invalid sku {unknown_sku}"
 
 
+@pytest.mark.usefixtures("postgres_db")
 @pytest.mark.usefixtures("restart_api")
-def test_deallocate(add_stock):
+def test_deallocate():
     sku, order1, order2 = random_sku(), random_orderid(), random_orderid()
     batch = random_batchref()
-    add_stock(
-        [(batch, sku, 100, "2011-01-02")]
-    )
+    put_to_add_batch(batch, sku, 100, "2011-01-02")
+
     url = config.get_api_url()
     # fully allocate
     r = requests.post(
@@ -75,14 +80,14 @@ def test_deallocate(add_stock):
 
     # deallocate
     r = requests.post(
-        f"{url}/deallocate", json={"orderid": order1, "sku": sku,},
+        f"{url}/deallocate", json={"orderid": order1, "sku": sku, },
     )
     assert r.ok
     assert r.json()["deallocated_batches"] == [batch]
 
     # deallocate again (the "unhappy path" is practically the same
     r = requests.post(
-        f"{url}/deallocate", json={"orderid": order1, "sku": sku,},
+        f"{url}/deallocate", json={"orderid": order1, "sku": sku, },
     )
 
     assert r.ok
