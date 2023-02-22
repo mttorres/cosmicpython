ed9e40d7170644a754a84af4c8c170159694f5e8
Arch <mateus.azeredo.torres@gmail.com>
2023-02-22 18:04:05 -0300

    Part 1 Part 1 TDD in High Gear and Low Gear: Creating a service layer test for allocate domain service making the domain easier to change.

--- tests/unit/test_services.py
+++ tests/unit/test_services.py
@@ -82,3 +82,16 @@ def test_deallocate_decrements_correct_quantity():
     assert batch.available_quantity == 100
     assert other_batch.available_quantity == 100
 
+
+def test_prefers_warehouse_batches_to_shipments():
+    in_stock_batch = Batch("in-stock-batch", "RETRO-CLOCK", 100, eta=None)
+    shipment_batch = Batch("shipment-batch", "RETRO-CLOCK", 100, eta=tomorrow)
+    repo = FakeRepository([in_stock_batch, shipment_batch])
+    session = FakeSession()
+
+    line = OrderLine('oref', "RETRO-CLOCK", 10)
+
+    services.allocate(line, repo, session)
+
+    assert in_stock_batch.available_quantity == 90
+    assert shipment_batch.available_quantity == 100
