5f44c488c6cf104b6668abaaa27580d23bdf3ba1
Arch <mateus.azeredo.torres@gmail.com>
2023-02-22 19:51:41 -0300

    Part 1 Part 1 TDD in High Gear and Low Gear: Implementation of add_batch endpoint that conforms to e2e tests

--- src/allocation/entrypoints/flask_app.py
+++ src/allocation/entrypoints/flask_app.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from flask import Flask, request
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -11,6 +13,24 @@ from src.allocation.service_layer import services
 orm.start_mappers()
 get_session = sessionmaker(bind=create_engine(config.get_postgres_uri()))
 app = Flask(__name__)
+
+
+@app.route("/batch", methods=["PUT"])
+def add_batch_endpoint():
+    session = get_session()
+    repo = repository.SqlAlchemyRepository(session)
+    eta = request.json["eta"]
+    if eta is not None:
+        eta = datetime.fromisoformat(eta).date()
+    services.add_batch(
+        request.json["ref"],
+        request.json["sku"],
+        request.json["qty"],
+        eta,
+        repo,
+        session,
+    )
+    return "OK", 201
 
 
 @app.route("/allocate", methods=["POST"])
