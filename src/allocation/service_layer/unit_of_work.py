cebad51317e809f9c4b5f14f0989bc2b75f453f0
Arch <mateus.azeredo.torres@gmail.com>
2023-02-24 23:56:10 -0300

    Part 1 UoW pattern: UoW SQLAlchemy implementation that conforms to our tests

--- src/allocation/service_layer/unit_of_work.py
+++ src/allocation/service_layer/unit_of_work.py
@@ -1,5 +1,8 @@
 from __future__ import annotations
 import abc
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
 
 from src.allocation import config
 from src.allocation.adapters import repository
@@ -23,3 +26,28 @@ class AbstractUnitOfWork(abc.ABC):
         raise NotImplementedError
 
 
+DEFAULT_SESSION_FACTORY = sessionmaker(
+    bind=create_engine(
+        config.get_postgres_uri(),
+    )
+)
+
+
+class SqlAlchemyUnitOfWork(AbstractUnitOfWork):
+    def __init__(self, session_factory=DEFAULT_SESSION_FACTORY):
+        self.session_factory = session_factory
+
+    def __enter__(self):
+        self.session = self.session_factory()
+        self.batches = repository.SqlAlchemyRepository(self.session)
+        return super().__enter__()
+
+    def __exit(self, *args):
+        super().__exit__(*args)
+        self.session.close()
+
+    def commit(self):
+        self.session.commit()
+
+    def rollback(self):
+        self.session.rollback()
