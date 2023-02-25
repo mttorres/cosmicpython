3a7aba215c4b95408dcbae077ae6c6b790b0c206
Arch <mateus.azeredo.torres@gmail.com>
2023-02-25 01:20:33 -0300

    Part 1 UoW pattern: Flask App refactor that conforms to our new UoW Layer

--- src/allocation/entrypoints/flask_app.py
+++ src/allocation/entrypoints/flask_app.py
@@ -1,24 +1,18 @@
 from datetime import datetime
 
 from flask import Flask, request
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 
-from src.allocation import config
+
 from src.allocation.domain import model
 from src.allocation.adapters import orm
-from src.allocation.adapters import repository
-from src.allocation.service_layer import services
+from src.allocation.service_layer import services, unit_of_work
 
 orm.start_mappers()
-get_session = sessionmaker(bind=create_engine(config.get_postgres_uri()))
 app = Flask(__name__)
 
 
 @app.route("/batch", methods=["PUT"])
 def add_batch_endpoint():
-    session = get_session()
-    repo = repository.SqlAlchemyRepository(session)
     eta = request.json["eta"]
     if eta is not None:
         eta = datetime.fromisoformat(eta).date()
@@ -27,20 +21,20 @@ def add_batch_endpoint():
         request.json["sku"],
         request.json["qty"],
         eta,
-        repo,
-        session,
+        unit_of_work.SqlAlchemyUnitOfWork()
     )
     return "OK", 201
 
 
 @app.route("/allocate", methods=["POST"])
 def allocate_endpoint():
-    db_session = get_session()
-    repo = repository.SqlAlchemyRepository(db_session)
 
     try:
         batchref = services.allocate(
-            request.json["orderid"], request.json["sku"], request.json["qty"], repo, db_session
+            request.json["orderid"],
+            request.json["sku"],
+            request.json["qty"],
+            unit_of_work.SqlAlchemyUnitOfWork()
         )
     except (model.OutOfStock, services.InvalidSku) as e:
         return {"message": str(e)}, 400
@@ -50,8 +44,7 @@ def allocate_endpoint():
 
 @app.route("/deallocate", methods=["POST"])
 def deallocate_endpoint():
-    db_session = get_session()
-    repo = repository.SqlAlchemyRepository(db_session)
+
     return {"deallocated_batches": services.deallocate(
-        request.json["orderid"], request.json["sku"], repo, db_session
+        request.json["orderid"], request.json["sku"], unit_of_work.SqlAlchemyUnitOfWork()
     )}, 200
