826e539d1b1a81d1061deb1e23b078af40c98fd2
Arch <mateus.azeredo.torres@gmail.com>
2023-03-03 19:24:25 -0300

    Part 1 Aggregates and Consistency Boundaries: Adding tests for our data integrity rules.

--- tests/integration/test_uow.py
+++ tests/integration/test_uow.py
@@ -1,12 +1,22 @@
 # pylint: disable=protected-access
+import threading
+import traceback
+from typing import List
 import pytest
+import time
 from sqlalchemy.sql import text
 
 from src.allocation.service_layer import unit_of_work
 from src.allocation.domain import model
+from tests.random_refs import random_sku, random_batchref, random_orderid
 
 
-def insert_batch(session, ref, sku, qty, eta):
+def insert_batch(session, ref, sku, qty, eta, product_version=1):
+    session.execute(text(
+        "INSERT INTO products (sku, version_number) VALUES (:sku, :version)"),
+        dict(sku=sku, version=product_version),
+    )
+
     session.execute(text(
         "INSERT INTO batches (reference, sku, _purchased_quantity, eta)"
         " VALUES (:ref, :sku, :qty, :eta)"),
@@ -25,6 +35,19 @@ def get_allocated_batch_ref(session, orderid, sku):
         dict(orderlineid=orderlineid),
     )
     return batchref
+
+
+def try_to_allocate(orderid, sku, exceptions):
+    line = model.OrderLine(orderid, sku, 10)
+    try:
+        with unit_of_work.SqlAlchemyUnitOfWork() as uow:
+            product = uow.products.get(sku=sku)
+            product.allocate(line)
+            time.sleep(0.2)
+            uow.commit()
+    except Exception as e:
+        print(traceback.format_exc())
+        exceptions.append(e)
 
 
 def test_uow_can_retrieve_a_batch_and_allocate_to_it(session_factory):
@@ -66,3 +89,39 @@ def test_rolls_back_on_error(session_factory):
     new_session = session_factory()
     rows = list(new_session.execute(text('SELECT * FROM "batches"')))
     assert rows == []
+
+
+def test_concurrent_updates_to_version_are_not_allowed(postgres_session_factory):
+    sku, batch = random_sku(), random_batchref()
+    session = postgres_session_factory()
+    insert_batch(session, batch, sku, 100, eta=None, product_version=1)
+    session.commit()
+
+    order1, order2 = random_orderid(1), random_orderid(2)
+    exceptions = []  # type: List[Exception]
+    # https://stackoverflow.com/questions/25010167/e731-do-not-assign-a-lambda-expression-use-a-def
+    thread1 = threading.Thread(target=lambda: try_to_allocate(order1, sku, exceptions))  # (1)
+    thread2 = threading.Thread(target=lambda: try_to_allocate(order2, sku, exceptions))  # (1)
+    thread1.start()
+    thread2.start()
+    thread1.join()
+    thread2.join()
+
+    [[version]] = session.execute(
+        "SELECT version_number FROM products WHERE sku=:sku",
+        dict(sku=sku),
+    )
+    assert version == 2
+    [exception] = exceptions
+    assert "could not serialize access due to concurrent update" in str(exception)
+
+    orders = session.execute(
+        "SELECT orderid FROM allocations"
+        " JOIN batches ON allocations.batch_id = batches.id"
+        " JOIN order_lines ON allocations.orderline_id = order_lines.id"
+        " WHERE order_lines.sku=:sku",
+        dict(sku=sku),
+    )
+    assert orders.rowcount == 1  # (4)
+    with unit_of_work.SqlAlchemyUnitOfWork() as uow:
+        uow.session.execute("select 1")
