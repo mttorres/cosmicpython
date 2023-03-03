bd1cb08c930f80385d8f791146ae128a9931deaf
Arch <mateus.azeredo.torres@gmail.com>
2023-03-03 18:49:20 -0300

    Part 1 Aggregates and Consistency Boundaries: Adding version_number to Product Aggregate.

--- src/allocation/domain/model.py
+++ src/allocation/domain/model.py
@@ -27,14 +27,16 @@ def deallocate(orderid: str, batches: List[Batch]):
 
 
 class Product:
-    def __init__(self, sku: str, batches: List[Batch]):
+    def __init__(self, sku: str, batches: List[Batch], version_number: int = 0):
         self.sku = sku
         self.batches = batches
+        self.version_number = version_number
 
     def allocate(self, line: OrderLine) -> str:
         try:
             batch = next(b for b in sorted(self.batches) if b.can_allocate(line))
             batch.allocate(line)
+            self.version_number += 1
             return batch.reference
         except StopIteration:
             raise OutOfStock(f"Out of stock for sku {line.sku}")
