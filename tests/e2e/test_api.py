004c451f4e499a782d77e9d5dfdd5e81914cdf93
Arch <mateus.azeredo.torres@gmail.com>
2023-04-13 19:47:36 -0300

    Part 2 CQRS: e2e tests for our new api version that uses the CQS principle.

--- tests/e2e/test_api.py
+++ tests/e2e/test_api.py
@@ -3,12 +3,13 @@ import pytest
 
 import src.allocation.config as config
 from tests.random_refs import random_sku, random_batchref, random_orderid
-from api_client import put_to_add_batch
+from api_client import put_to_add_batch, post_to_allocate, get_allocation
 
 
 @pytest.mark.usefixtures("postgres_db")
 @pytest.mark.usefixtures("restart_api")
-def test_happy_path_returns_201_and_allocated_batch():
+def test_happy_path_returns_202_and_batch_is_allocated():
+    orderid = random_orderid()
     sku, othersku = random_sku(), random_sku("other")
     earlybatch = random_batchref("1")
     laterbatch = random_batchref("2")
@@ -18,21 +19,25 @@ def test_happy_path_returns_201_and_allocated_batch():
     put_to_add_batch(otherbatch, othersku, 100, None)
     data = {"orderid": random_orderid(), "sku": sku, "qty": 3}
 
-    url = config.get_api_url()
-    r = requests.post(f"{url}/allocate", json=data)
+    r = post_to_allocate(orderid, sku, qty=3)
+    assert r.status_code == 202
 
-    assert r.status_code == 201
-    assert r.json()["batchref"] == earlybatch
+    r = get_allocation(orderid)
+    assert r.ok
+    assert r.json() == [
+        {"sku": sku, "batchref": earlybatch}
+    ]
 
 
 @pytest.mark.usefixtures("restart_api")
 def test_unhappy_path_returns_400_and_error_message():
     unknown_sku, orderid = random_sku(), random_orderid()
-    data = {"orderid": orderid, "sku": unknown_sku, "qty": 20}
-    url = config.get_api_url()
-    r = requests.post(f"{url}/allocate", json=data)
+    r = post_to_allocate(orderid, unknown_sku, qty=20, expect_success=False)
     assert r.status_code == 400
     assert r.json()["message"] == f"Invalid sku {unknown_sku}"
+
+    r = get_allocation(orderid)
+    assert r.status_code == 404
 
 
 @pytest.mark.usefixtures("postgres_db")
