0b7b69c9be2a0eefb6c530aaa9818fc164f9bf50
Arch <mateus.azeredo.torres@gmail.com>
2023-05-23 20:36:22 -0300

    Part 2 Event-Driven Architecture Exercise: Test Driving our new change to allow Allocate commands .

--- tests/e2e/test_external_events.py
+++ tests/e2e/test_external_events.py
@@ -6,6 +6,19 @@ from tests.random_refs import random_sku, random_orderid, random_batchref
 import api_client
 
 
+def check_allocated_event(batchref, orderid, subscription):
+    messages = []
+    for attempt in Retrying(stop=stop_after_delay(3), reraise=True):
+        with attempt:
+            message = subscription.get_message(timeout=1)
+            if message:
+                messages.append(message)
+                print(messages)
+            data = json.loads(messages[-1]["data"])
+            assert data["orderid"] == orderid
+            assert data["batchref"] == batchref
+
+
 def test_change_batch_quantity_leading_to_reallocation():
     # start with two batches and an order allocated to one of them
     orderid, sku = random_orderid(), random_sku()
@@ -22,13 +35,24 @@ def test_change_batch_quantity_leading_to_reallocation():
     )
 
     # wait until we see a message saying the order has been reallocated
-    messages = []
-    for attempt in Retrying(stop=stop_after_delay(3), reraise=True):
-        with attempt:
-            message = subscription.get_message(timeout=1)
-            if message:
-                messages.append(message)
-                print(messages)
-                data = json.loads(messages[-1]["data"])
-                assert data["orderid"] == orderid
-                assert data["batchref"] == later_batch
+    check_allocated_event(later_batch, orderid, subscription)
+
+
+def test_allocate_external_command_leads_to_allocation():
+    # start with two batches a
+    orderid, sku = random_orderid(), random_sku()
+    earlier_batch, later_batch = random_batchref("old"), random_batchref("newer")
+    api_client.put_to_add_batch(earlier_batch, sku, qty=10, eta="2011-01-01")
+    api_client.put_to_add_batch(later_batch, sku, qty=10, eta="2011-01-02")
+
+    subscription = redis_client.subscribe_to("line_allocated")
+    # issue allocation
+    redis_client.publish_message(
+        "allocate_line",
+        {"orderid": orderid,
+         "sku": sku,
+         "qty": 10},
+    )
+
+    # wait until we see a message...
+    check_allocated_event(earlier_batch, orderid, subscription)
